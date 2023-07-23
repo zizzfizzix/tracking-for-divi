abf59bd5572e35c945fb0910f1d9348823bb810a
Kuba Serafinowski <kuba.serafinowski@gmail.com>
2023-07-23 20:55:07 +0100

    chore: Improve comments

--- js/src/lib/findFormId.ts
+++ js/src/lib/findFormId.ts
@@ -1,2 +1,3 @@
+// The form payload keys include a form ID suffix which is a "0" by default, but we define it just in case.
 export const findFormId = (objKey: string) =>
   /.+_([^_]+)/.exec(objKey)?.[1] ?? "0";
--- js/src/lib/findKeyInObject.ts
+++ js/src/lib/findKeyInObject.ts
@@ -1,2 +1,3 @@
+// The form payload keys include a suffix form ID which is an unknown so we need to find the correct keys based on the known format
 export const findKeyInObject = (object: Record<string, any>, query: string) =>
   Object.keys(object).filter((key) => key.startsWith(query))[0];
